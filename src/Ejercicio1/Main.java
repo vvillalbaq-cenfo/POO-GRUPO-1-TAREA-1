f43c1162abac82b6506b1777204d1645f16e4c2c
Victor Villalba <vvillalbaq@ucenfotec.ac.cr>
2025-10-02 17:53:48 +0000

    V10

--- src/Ejercicio1/Main.java
+++ src/Ejercicio1/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         /* Creacion de los tres tipos de Motores **/
         MElectrico motorE = new MElectrico("Toshiba", "EGP5", 5000, 2.5f);
-        MCGasolina motorG = new MCGasolina("Nissan", "N300", 3500, 95f);
-        MCDiesel motorD = new MCDiesel("Cummis", "C789", 25000, 150f);
+        MCGasolina motorG = new MCGasolina("Nissan", "N300", 3500, 98f);
+        MCDiesel motorD = new MCDiesel("Cummis", "C789", 25000, 200f);
 
         /* Impresion de los atributos de los motores **/
         System.out.println(motorE);
@@ -13,9 +13,9 @@ public class Main {
         System.out.println(motorD);
 
         /* Cambio de Atributos mediante el flotante **/
-        motorE.setAlgo(350.0f);
-        motorG.setAlgo(98.0f);
-        motorD.setAlgo(200.0f);
+        motorE.setAlgo(1.5f);
+        motorG.setAlgo(90f);
+        motorD.setAlgo(180.0f);
 
         /* Impresion de los atributos de los motores despues del cambio **/
         System.out.println("Modificancion de atributos");
